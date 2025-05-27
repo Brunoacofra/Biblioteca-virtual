7109e6b5795b14bd27b0b4ae0007fbe20a2a46a1
Bruno <119612459+Brunoacofra@users.noreply.github.com>
2025-05-27 05:25:29 -0300

    Inicio

---
+++ cadastroAutor.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Biblioteca Virtual</title>
+	</head>
+	<body>
+		<form method="post">
+			<label>Nome do autor : </label>
+			<input type="text" name="name">
+			<button type="submit">Enviar</button>
+		</form>
+	</body>
+</html>
---
+++ cadastroGenero.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Biblioteca Virtual</title>
+	</head>
+	<body>
+		<form method="post">
+			<label>Genero:</label>
+			<input type="text" name="genero">
+			<button type="submit">Enviar</button>
+		</form>
+	</body>
+</html>
